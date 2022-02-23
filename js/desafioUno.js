544f074903c048092fd710e1915f2d9837b66caa
Matias <matias1b2015@gmail.com>
2022-02-22 23:40:28 -0300

    finalizado por ahora

--- js/desafioUno.js
+++ js/desafioUno.js
@@ -13,6 +13,5 @@ var notaQuimica = prompt("Ingrese la nota de Quimica");
 var notaBiologia = prompt("Ingrese la nota Biologia");
 
 var notaHistoria = prompt("Ingrese la nota Historia");
-
-var promedio = (parseInt (notaIngles) + parseInt (notaMatematica) + parseInt (notaFisica) + parseInt (notaQuimica) + parseInt (notaHistoria) + parseInt (notaBiologia)) /6;
+var promedio=( parseInt(notaIngles) +  parseInt(notaMatematica) + parseInt(notaFisica) +  parseInt(notaQuimica) +  parseInt(notaHistoria) +  parseInt(notaBiologia)) /6;
     alert("Tu promedio es: " + promedio) 
