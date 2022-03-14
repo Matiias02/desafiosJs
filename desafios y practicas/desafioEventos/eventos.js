50ce4738b416ca15dcb9736ddb86256c62db3484
Matias <matias1b2015@gmail.com>
2022-03-14 17:00:28 -0300

    desafio eventos

--- desafios y practicas/desafioEventos/eventos.js
+++ desafios y practicas/desafioEventos/eventos.js
@@ -2,7 +2,10 @@
 //     alert("Agregaste este producto al carrito");
 // }
 
-function (){
-    
+function abrir(){
+    document.getElementById("ventana") . style.display="block"
 }
-    
+
+function cerrar(){
+    document.getElementById("ventana") . style.display="none"
+}
